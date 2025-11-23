5d2e6a4a4c989c77aabcbf8acd70724e7eab1169
kp1153 <kamta_tiwari@yahoo.com>
2025-11-23 17:59:43 +0530

    force deploy

--- app/[category]/[slug]/page.js
+++ app/[category]/[slug]/page.js
@@ -80,7 +80,7 @@ const portableTextComponents = {
     link: ({ value, children }) => {
       const href = value?.href || "#";
       return (
-        
+        <a
           href={href}
           className="text-blue-600 hover:text-blue-800 underline font-medium"
           target="_blank"
